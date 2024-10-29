d276109f7f4e181035451bcbd7b6534d1369fe99
lazarus0320 <astar5327z@gmail.com>
2024-10-29 14:40:34 +0900

    SCRUM-1/Test:1

--- src/main/java/org/example/sharesportsbackend/ShareSportsBackendApplication.java
+++ src/main/java/org/example/sharesportsbackend/ShareSportsBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShareSportsBackendApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(ShareSportsBackendApplication.class, args);
 	}
